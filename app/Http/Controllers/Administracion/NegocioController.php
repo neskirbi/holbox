ee72221c7470c371090e26cd7c0ccd77b12e06e3
Raul <neskirbi@gmail.com>
2022-10-25 08:59:00 -0500

    Arreglando alerta

--- app/Http/Controllers/Administracion/NegocioController.php
+++ app/Http/Controllers/Administracion/NegocioController.php
@@ -195,7 +195,7 @@ class NegocioController extends Controller
         $negocio->correo=$request->correo;
 
         $confi=Configuracion::select('iva')->where('id_planta',$request->planta)->first();
-        $negocio->iva=$confi->iva;
+        $negocio->iva=16;
         
 
         if($negocio->save()){
