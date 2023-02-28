69f0efc1113c775fbda66b0c547c72f9b4e3c6b4
Raul <neskirbi@gmail.com>
2023-02-28 13:50:10 -0600

    Agregue terminos y condiciones

---
+++ resources/views/formatos/terminosycondiciones.blade.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Términos y Condiciones</title>
+</head>
+<body>
+   
+    
+    
+    
+    <center>
+        <b>
+            TÉRMINOS Y CONDICIONES DE USO DE LA APLICACIÓN “RECI-TRASH”
+
+        </b>
+    </center>
+    
+    
+    <p align="justify">
+            
+        Ésta aplicación ha sido diseñada y administrada por “RECI-TRASH” en adelante <b>“El Proveedor”</b>, así como los módulos que conforman ésta aplicación han sido diseñados para integrar información de utilidad que pueda ser consultada desde un dispositivo móvil, como lo es información relativa a Planes de manejo de Residuos de la Construcción y Demolición, Residuos Sólidos Urbanos y/o Residuos de Manejo Especial en adelante <b>“Residuos”</b>.
+
+
+    </p>
+    <p align="justify">
+        
+        Cualquier persona que desee hacer uso de los servicios que ofrece esta aplicación móvil, podrá hacerlo sujetándose a los presentes <b>“Términos y Condiciones de Uso” </b>así como a las políticas y principios que se describen en el presente documento.
+
+
+    </p>
+    
+    <b><p>1. OBJETO</p></b>
+   
+    <p align="justify">
+        
+        Este documento describe los términos y condiciones generales de uso de la aplicación móvil en adelante <b>“RECI-TRASH”</b>, misma que es aplicación para dispositivos móviles que integra en una sola aplicación el acceso a los distintos módulos por los que está conformada; a través de los cuales, la Secretaría del Medio Ambiente, Secretaría de Movilidad, Secretaría de Obras y Servicios, Secretaría de Seguridad Pública, Secretaría de la Contraloría General, Procuraduría Ambiental del Ordenamiento Territorial, Instituto de Verificación Administrativa, las Alcaldías y/o Municipios dentro del territorio Nacional en adelante <b>“Autoridad Competente”</b> y <b>“El Proveedor”</b> pondrán a disposición trámites y servicios a los Generadores de Residuos en adelante <b>“El Usuario”</b>.
+
+
+    </p>
+    
+    
+    <p>Los servicios mencionados en el párrafo anterior son:</p>
+    
+    
+    <ol type="a"> 
+        <li>Trazabilidad de Residuos Generados en tiempo real</li>
+        <li>Gestión de Residuos</li>
+        <li>Administración de Residuos</li>
+        <li>Generación de Citas ante el destino final para el correcto depósito de los Residuos en los Puntos de Disposición Final</li>
+        <li>Generación de Contrato de Prestación de Servicios con el Punto de Disposición Final</li>
+        <li>Verificación de Pagos</li>
+        <li>Verificación por parte de la <b>“Autoridad Competente”</b> del cumplimiento de los planes de manejo de Residuos reportados por los centros autorizados de disposición final y <b>“El Usuario”</b>.</li>
+    </ol>       
+    </p>
+    
+    
+    <p><b>2. ACEPTACIÓN DE LOS TÉRMINOS</b></p>
+    
+    
+    <p align="justify">
+        
+        El ingreso y utilización de la aplicación “RECI-TRASH” implica que usted ha leído, entendido y aceptado los presentes “Términos y Condiciones de Uso”.
+        
+    </p>
+    
+    
+    <p><b>3. RESPONSABILIDAD SOBRE EL USO DE LA APLICACIÓN “RECI-TRASH”</b></p>
+    
+    
+    <p align="justify">
+        
+        <b>“El usuario”</b> asume la responsabilidad de todas las acciones que ejecute, en la utilización y consulta de <b>“RECI-TRASH”</b>. Usted no utilizará, ni permitirá que terceros utilicen esta aplicación móvil o cualquier servicio prestado a través de ella, de forma contraria a lo establecido en estos “Términos y Condiciones de Uso”, así como a las disposiciones legales aplicables a los servicios que ofrece la aplicación <b>“RECI-TRASH”</b>. Es responsabilidad de <b>“El usuario”</b> todo acto que se realice en esta aplicación móvil, por medio de su “Nombre de Usuario” y “Contraseña”; asimismo, usted se responsabiliza del cuidado y custodia de los mismos, para que esta aplicación móvil únicamente sea utilizada para los fines para los cuales ha sido diseñada, por lo que cualquier mal uso que se realice en su nombre, se presumirá realizada por usted.
+    </p>
+    
+    
+    <p align="justify">
+        
+        <b>“El usuario”</b> reconoce y acepta que <b>“El Proveedor”</b> queda liberado de toda responsabilidad por el mal uso que llegara a realizarse en alguno de los módulos de <b>“RECI-TRASH”</b>. Como todo desarrollo tecnológico, <b>“RECI-TRASH”</b> es falible y estará en proceso permanente de mejora; por lo que existe la posibilidad, aunque remota, de que ocurran fallas técnicas durante su uso; por lo que <b>“El Proveedor”</b> no garantiza que la aplicación móvil esté libre de errores; por tal motivo, usted deslinda de toda responsabilidad a <b>“El Proveedor”</b>, respondiendo de todos los daños y perjuicios derivados de las circunstancias expuestas en este párrafo.
+    </p>
+    
+    
+    <p align="justify">
+        
+        <b>“El Proveedor”</b> no asumirá ninguna responsabilidad por los inconvenientes que <b>“El usuario”</b> pudiera experimentar con el equipo de cómputo, dispositivo móvil y/o accesorios (hardware y software) utilizados para conectarse a esta aplicación móvil; de igual manera, tampoco asumirá responsabilidad alguna por procedimientos que hayan quedado inconclusos. En ningún caso <b>“El Proveedor”</b> será responsable por las consecuencias del uso indebido o fraudulento de <b>“RECI-TRASH”</b>, cualquiera que sea la causa del eventual daño.
+
+
+    </p>
+    
+    
+    <p><b>4. CAMBIOS Y ACTUALIZACIONES DEL SERVICIO</p></b>
+    
+    
+    <p align="justify">
+        
+        <b>“El Proveedor”</b> tiene pleno derecho de realizar modificaciones en los contenidos que se ofrecen, ya sean temporales o permanentes, en cualquier momento, por lo tanto, no es posible garantizar la disponibilidad ni la continuidad del funcionamiento de <b>“RECI-TRASH”</b> en todo momento o durante el tiempo de actualización. La <b>“Autoridad Competente”</b> y/o <b>“El Proveedor”</b> no asume responsabilidad alguna por cualquier falla que pudiera presentarse con el uso de esta aplicación, ya sean por cambios y actualizaciones, o por cualquiera otra causa.
+
+
+    </p>
+    
+    
+    <p align="justify">
+        
+        <b>“El Proveedor”</b> se reserva el derecho de modificar, restringir o suprimir todos o cualquiera de los atributos de los módulos de <b>“RECI-TRASH”</b>, en forma temporal o definitiva, sin que estas medidas puedan ser objeto de requerimiento alguno, ni de derecho a reclamar daños y perjuicios por parte de los usuarios.
+
+    </p>
+    
+    
+    <p><b>5. POLÍTICAS DE PRIVACIDAD Y PROTECCIÓN DE DATOS PERSONALES</b></p>
+    
+    
+    <p align="justify">
+    
+        El tratamiento de sus datos personales se realizará de conformidad con lo establecido en los lineamientos y la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, por lo cual el usuario está consciente que su información, todos los datos ingresados en <b>“RECI-TRASH”</b> así como los Planes de Manejo de Residuos reportados ante la autoridad competente pueden ser compartidos por la <b>“Autoridad Competente”</b> con <b>“El Proveedor”</b>, así mismo el proveedor podrá compartir la información con terceros, ya sean entidades privadas o públicas y personas físicas o morales para fines de Marketing, Consultoría u otros servicios.
+    </p>
+    
+    
+    <p align="justify">
+        
+        El Usuario autoriza que toda información ingresada en <b>“RECI-TRASH”</b> será compartida por <b>“El Proveedor”</b> a través de su usuario y contraseña, liberando de toda responsabilidad penal y/o administrativa a él proveedor.
+    </p>
+    
+    
+    <p><b>6. POLÍTICA DE PROPIEDAD INTELECTUAL</b></p>
+    
+    
+    <p align="justify">
+        
+        <b>“RECI-TRASH”</b> es una obra protegida por las leyes mexicanas, internacionales y tratados en materia
+        de propiedad intelectual.
+    </p>
+    
+    
+    <p align="justify">
+        
+        El uso de <b>“RECI-TRASH”</b>, no lo convierte a usted en titular de ninguno de los derechos de propiedad
+        intelectual del mismo, ni del contenido o información a la que acceda. Queda prohibido utilizar el
+        nombre, la marca o el logotipo del mismo. Asimismo, no se podrá eliminar, ocultar ni alterar los avisos
+        legales que se muestran en la misma. Sin perjuicio de lo anterior, usted podrá imprimirlos, copiarlos o
+        almacenarlos, siempre y cuando sea para uso estrictamente personal.
+    </p>
+    
+    
+    <p><b>7. CONTROVERSIAS Y JURISDICCIÓN APLICABLE.</b></p>
+
+    <p align="justify">
+        “El Proveedor” se reserva el derecho de presentar acciones civiles o penales cuando se advierta el uso indebido de la información contenida en<b>“RECI-TRASH”</b>, o por el incumplimiento de los presentes <b>“TÉRMINOS Y CONDICIONES DE USO”</b>.
+    </p>
+    
+    
+    <p align="justify">
+    
+    La relación entre <b>“El Proveedor”</b> y <b>“El usuario”</b> se regirá por la legislación vigente en la Ciudad de México, y la que sea aplicable en el ámbito federal. En caso de existir controversia legal, las partes se sujetarán a la jurisdicción de los tribunales de la Ciudad de México, conforme a la materia que corresponda.
+    </p>
+
+</body>
+
+</html>
